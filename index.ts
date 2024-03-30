91ef7a1d3fe96689d8a4432ccbb0a796588e529f
Victor Spichenkoff <victorspich13@gmail.com>
2024-03-30 11:32:24 -0300

    Projeto criado

---
+++ index.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+const app = express() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(2006, () => console.log('Rodando na porta 2006'))
